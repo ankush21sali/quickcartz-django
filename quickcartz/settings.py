78ec113cd29f43980c6941bb12dc70f08af6592c
ankush21sali <saliankush21@.com>
2025-12-30 21:18:20 +0530

    Deploy On Railway!

--- quickcartz/settings.py
+++ quickcartz/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["quickcartz.up.railway.app"]
+ALLOWED_HOSTS = [
+    "quickcartz.up.railway.app",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -176,3 +181,9 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+
+CSRF_TRUSTED_ORIGINS = ["https://quickcartz.up.railway.app",]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
