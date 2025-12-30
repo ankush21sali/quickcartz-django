2774b6718ba9e986cbf93bb9a24596320b3e3a2d
ankush21sali <saliankush21@.com>
2025-12-30 22:29:23 +0530

    Deploy On Railway!

--- quickcartz/settings.py
+++ quickcartz/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,6 +116,12 @@ else:
         'PORT': config('DB_PORT', cast=int),
         }
     }
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=config("DATABASE_URL")
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
