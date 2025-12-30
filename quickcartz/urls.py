02feebdb665ff72c0a6dfaae16879661a3664f6e
ankush21sali <saliankush21@.com>
2025-12-30 13:41:09 +0530

    Add Cloudinary media storage!

--- quickcartz/urls.py
+++ quickcartz/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
