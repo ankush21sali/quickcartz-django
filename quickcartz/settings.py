43efb42065d84881b0ab1478b09e57e4b26f5d57
ankush21sali <saliankush21@.com>
2025-12-31 17:38:13 +0530

    Deploy on Render

--- quickcartz/settings.py
+++ quickcartz/settings.py
@@ -200,6 +200,7 @@ MESSAGE_TAGS = {
 
 
 # SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
