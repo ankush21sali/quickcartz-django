e1a4b8ebf4edc42100bd9b9b81dd53a45610151f
ankush21sali <saliankush21@.com>
2026-02-05 18:21:50 +0530

    Sqlite db Configuration!

--- quickcartz/settings.py
+++ quickcartz/settings.py
@@ -126,11 +126,11 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 # Render Live PostgreSQL DB
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=config("DATABASE_URL")
-    )
-}
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=config("DATABASE_URL")
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
